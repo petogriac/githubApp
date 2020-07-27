42885dd88743875aa697b79afb4b06075dda45f0
pg754t <pg754t@intl.att.com>
2020-07-27 21:28:08 +0200

    Adding not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'users' }];
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'users' }, {path: '**', redirectTo: 'users'}];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
